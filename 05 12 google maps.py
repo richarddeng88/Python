e8116b84fe695488c189744f95ae2f7bf81f2d40
Richard <richarddeng88@gmail.com>
2016-05-13 13:15:15 -0400

    0513

--- 05 12 google maps.py
+++ 05 12 google maps.py
@@ -1,63 +1,20 @@
-import googlemaps
-from datetime import datetime
-gmaps = googlemaps.Client(key='AIzaSyDTV6vS1GeEk_pR5ocM_ClRgOSBc1my4oo')
+from googlemaps import Client
 
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+c = Client(key="AIzaSyDTV6vS1GeEk_pR5ocM_ClRgOSBc1my4oo")
 
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+d = c.directions((40.7529737,-73.9925333),(40.7498048,-74.0148914))
 
-# Request directions via public transit
-now = datetime.now()
-result = gmaps.directions("Sydney Town Hall","Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
-
-
-result = gmaps.directions((40.7529737,-73.9925333),
-                                         (40.7498048,-74.0148914),
-                                     mode="transit",
-                                     departure_time=now)
-
-## find the optimized steps, time consumed, and distance
-info = gmaps.directions((40.7529737,-73.9925333),(40.7498048,-74.0148914),
-                        mode='bicycling',
-                        departure_time=now)[0]["legs"][0]
-                        # this function 'legs' only include steps, duration, distance three factors. 
-steps = info["steps"]
-duration = info["duration"]['value']
-distance = info['distance']['value']
+steps = c.directions("texarkana","atlanta")[0]["legs"][0]["steps"]
 
 for d in steps:
     print(d['html_instructions'])
-for d in steps:
-    print(d['end_location'],d['duration']['value'])
-for d in steps:
-    print(d['duration']['value'])
-for d in steps:
-    print(d['travel_mode'])
-
-    
-a = "Toal time used is "+ str(duration) +" seconds"
-print(a)
-b = "The distance is " + str(distance) + " meters"
-print b
-
-## calcuate the optimized geo distance 
-distance = gmaps.distance_matrix((40.7529737,-73.9925333),(40.7498048,-74.0148914))
-#print(distance['status'])
-#print(distance['rows'])
-#print(distance['origin_addresses'])
-#print(distance['destination_addresses'])
-dist=distance['rows'][0]['elements'][0]['distance']['value']
 
 
+from googlemaps import Client
+a = geocode(Client(key="AIzaSyDTV6vS1GeEk_pR5ocM_ClRgOSBc1my4oo"),
+        "4135 Elbertson St, Elmhurst, NY,11373")
+a = geocode(Client(key="AIzaSyDTV6vS1GeEk_pR5ocM_ClRgOSBc1my4oo"),
+        (-73.875782,40.746503))
 
-
-
-
-
-
-
-
+a = distance_matrix(Client(key="AIzaSyDTV6vS1GeEk_pR5ocM_ClRgOSBc1my4oo"),
+                    (40.7529737,-73.9925333),(40.7498048,-74.0148914))
