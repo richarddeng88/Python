f75fcf28f7c78e2b92690f59c6971266d3e1a89d
Richard <richarddeng88@gmail.com>
2016-05-13 13:14:38 -0400

    05 13

--- 05 12 google maps.py
+++ 05 12 google maps.py
@@ -10,8 +10,7 @@ reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
 # Request directions via public transit
 now = datetime.now()
-result = gmaps.directions("Sydney Town Hall",
-                                          "Parramatta, NSW",
+result = gmaps.directions("Sydney Town Hall","Parramatta, NSW",
                                      mode="transit",
                                      departure_time=now)
 
@@ -21,25 +20,39 @@ result = gmaps.directions((40.7529737,-73.9925333),
                                      mode="transit",
                                      departure_time=now)
 
-## find the optimized steps and time consumed
-steps = gmaps.directions((40.7529737,-73.9925333),(40.7498048,-74.0148914))[0]["legs"][0]["steps"]
-duration = gmaps.directions((40.7529737,-73.9925333),
-                            (40.7498048,-74.0148914))[0]["legs"][0]["duration"]['value']
+## find the optimized steps, time consumed, and distance
+info = gmaps.directions((40.7529737,-73.9925333),(40.7498048,-74.0148914),
+                        mode='bicycling',
+                        departure_time=now)[0]["legs"][0]
+                        # this function 'legs' only include steps, duration, distance three factors. 
+steps = info["steps"]
+duration = info["duration"]['value']
+distance = info['distance']['value']
+
 for d in steps:
     print(d['html_instructions'])
 for d in steps:
-    print(d['end_location'])
-a = "time consumed is "+ str(duration) +" seconds"
+    print(d['end_location'],d['duration']['value'])
+for d in steps:
+    print(d['duration']['value'])
+for d in steps:
+    print(d['travel_mode'])
+
+    
+a = "Toal time used is "+ str(duration) +" seconds"
 print(a)
-## calcuate the optimized geo distance 
-distance = gmaps.distance_matrix((41.7529737,-73.9925333),(40.7498048,-74.0148914))
-print(distance['status'])
-print(distance['rows'])
-print(distance['origin_addresses'])
-print(distance['destination_addresses'])
-dist=distance['rows'][0]['elements'][0]['distance']['value']
-b = "the distance is " + str(dist) + " meters"
+b = "The distance is " + str(distance) + " meters"
 print b
+
+## calcuate the optimized geo distance 
+distance = gmaps.distance_matrix((40.7529737,-73.9925333),(40.7498048,-74.0148914))
+#print(distance['status'])
+#print(distance['rows'])
+#print(distance['origin_addresses'])
+#print(distance['destination_addresses'])
+dist=distance['rows'][0]['elements'][0]['distance']['value']
+
+
 
 
 
