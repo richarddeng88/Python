b0757cbd63a5d3e618055cd2720308355000d8c4
Richard <richarddeng88@gmail.com>
2016-05-30 10:55:19 -0400

    05 30

---
+++ 05 30 matplotlib basic 2.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+
+##### loading data from files
+# x,y = np.loadtxt('example.txt', delimiter=',', unpack=True)
+#
+# plt.plot(x,y, label= 'loading data from files')
+#
+# plt.title('loading data from files')
+# plt.legend()
+# plt.show()
+
+### loading data from internet
+
+# def gra(stock):
+#     url = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+ '/chartdata;type=quote;range=10y/csv'
+#     source_code = urllib.request.urlopan(url).read().decode()
+#     stock_data =[]
+#     split = source_code.split('\n')
+#
+#     for line in split:
+#
+#
+# gra('TSLA')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
