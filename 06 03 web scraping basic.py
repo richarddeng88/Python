b0b4e170a1b26b64809496c5bbdbd32b16a8c1ee
Richard <richarddeng88@gmail.com>
2016-06-04 22:55:20 -0400

    06 03

--- 06 03 web scraping basic.py
+++ 06 03 web scraping basic.py
@@ -1,6 +1,8 @@
 import urllib
 import re
 
+## in this example, actually we request the entire page and then extract some details from the entire source.
+
 urls = ["http://google.com",
         "http://cnn.com",
         "http://nytimes.com",
@@ -18,14 +20,44 @@ urls = ["http://google.com",
 #     print titles
 #     i+=1
 
-symbol_list = ["aapl","spy","goog","nflx",'amzn']
-i=0
-while i < len(symbol_list):
-    urls = "http://finance.yahoo.com/q?s="+ symbol_list[i]+ "&ql=0"
+
+# symbol_list = ["aapl","spy","goog","nflx",'amzn']
+# i=0
+# while i < len(symbol_list):
+#     urls = "http://finance.yahoo.com/q?s="+ symbol_list[i]+ "&ql=0"
+#     htmlfile = urllib.urlopen(urls)
+#     htmltext = htmlfile.read()
+#     regex = '<span id="yfs_l84_'+symbol_list[i]+'">(.+?)</span>'
+#     pattern = re.compile(regex)
+#     price = re.findall(pattern, htmltext)
+#     print "the price of ", symbol_list[i],"is", price[0]
+#     i += 1
+
+
+
+symbol_file = open("symbol.txt")
+symbol_list = symbol_file.read()
+symbol_list = symbol_list.split("\n")
+print symbol_list
+
+# for i in symbol_list[1:100]:
+#     urls = "http://finance.yahoo.com/q?s="+ i + "&ql=0"
+#     htmlfile = urllib.urlopen(urls)
+#     htmltext = htmlfile.read()
+#     regex = '<span id="yfs_l84_'+ i +'">(.+?)</span>'
+#     pattern = re.compile(regex)
+#     price = re.findall(pattern, htmltext)
+#     print "the price of ", i ,"is", price[0]
+
+j=1
+for i in symbol_list[1:200]:
+    urls = "http://finance.yahoo.com/q?s="+ i + "&ql=0"
     htmlfile = urllib.urlopen(urls)
     htmltext = htmlfile.read()
-    regex = '<span id="yfs_l84_'+symbol_list[i]+'">(.+?)</span>'
+    regex = '<span id="yfs_l84_[^.]*">(.+?)</span>'  # any number of character, either upper case or lower case can be accessed here.
     pattern = re.compile(regex)
     price = re.findall(pattern, htmltext)
-    print "the price of ", symbol_list[i],price
-    i += 1
+    print j, "the price of ", i ,"is", price # price[0]
+    j+=1
+
+
