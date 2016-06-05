9dd98447c0b956562aef97959c9009c35f86b8cd
Richard <richarddeng88@gmail.com>
2016-06-05 00:02:31 -0400

    06 04

--- 06 03 web scraping basic.py
+++ 06 03 web scraping basic.py
@@ -1,5 +1,6 @@
 import urllib
 import re
+import json
 
 ## in this example, actually we request the entire page and then extract some details from the entire source.
 
@@ -38,7 +39,7 @@ urls = ["http://google.com",
 symbol_file = open("symbol.txt")
 symbol_list = symbol_file.read()
 symbol_list = symbol_list.split("\n")
-print symbol_list
+
 
 # for i in symbol_list[1:100]:
 #     urls = "http://finance.yahoo.com/q?s="+ i + "&ql=0"
@@ -49,15 +50,46 @@ print symbol_list
 #     price = re.findall(pattern, htmltext)
 #     print "the price of ", i ,"is", price[0]
 
-j=1
-for i in symbol_list[1:200]:
-    urls = "http://finance.yahoo.com/q?s="+ i + "&ql=0"
-    htmlfile = urllib.urlopen(urls)
-    htmltext = htmlfile.read()
-    regex = '<span id="yfs_l84_[^.]*">(.+?)</span>'  # any number of character, either upper case or lower case can be accessed here.
-    pattern = re.compile(regex)
-    price = re.findall(pattern, htmltext)
-    print j, "the price of ", i ,"is", price # price[0]
-    j+=1
+# j=1
+# for i in symbol_list[1:20]:
+#     urls = "http://finance.yahoo.com/q?s="+ i + "&ql=0"
+#     htmlfile = urllib.urlopen(urls)
+#     htmltext = htmlfile.read()
+#     regex = '<span id="yfs_l84_[^.]*">(.+?)</span>'  # any number of character, either upper case or lower case can be accessed here.
+#     pattern = re.compile(regex)
+#     price = re.findall(pattern, htmltext)
+#     print j, "the price of ", i ,"is", price # price[0]
+#     j+=1
+
+#### downloading 1 min stock price of appl
+# htmltext = urllib.urlopen("http://www.bloomberg.com/markets/api/bulk-time-series/price/AAPL%3AUS?timeFrame=1_DAY")
+# data = json.load(htmltext)
+# print "appl 1 min data"
+# print data[0]["price"][2]["value"]
+# datapoint = data[0]["price"]
+# # for point in datapoint:
+# #     print point["value"]
+# print "the total number of stock value are ", len(datapoint)
+
+symbol_list = open("symbol.txt").read()
+symbol_list = symbol_list.split("\n")
+
+for symbol in symbol_list:
+    print symbol
+    myfile = open("bloomberg_stock_price/"+ symbol +".txt", "w+" )
+    myfile.close()
+
+    htmltext = urllib.urlopen("http://www.bloomberg.com/markets/api/bulk-time-series/price/"+ symbol +"%3AUS?timeFrame=1_DAY")
+    data = json.load(htmltext)
+    # print data[0]["price"]
+    datapoint = data[0]["price"]
+
+    myfile = open("bloomberg_stock_price/" + symbol + ".txt", "a")
+    for point in datapoint:
+        #print "at ",point["dateTime"],", the price is ",point["value"]
+        myfile.write(str(symbol +"," + str(point["dateTime"]) +","+ str(point["value"])+"\n"))
+        #myfile.close()
+
+
 
 
