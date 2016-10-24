23bb85b73226febb408de1fc75327b090d41c39d
Richard <richarddeng88@gmail.com>
2016-10-24 18:02:26 -0400

    tweet streaming

--- 4. web scraping + twitter analysis/3. twitter straming.py
+++ 4. web scraping + twitter analysis/3. twitter straming.py
@@ -2,10 +2,15 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
-ckey = "ejRTMayX2nhKZB2yNDCjDJWHz"
-csecret = "3PGjS2HGvtEPY1DZh9feUDcMv0O02VyYk7Q5MLIS5MzibxwtuD"
-atoken = "3045806279-RTecSDiDhevDXJ2YnbZ7BJKvUSylb6VvoDQqzDC"
-asecret = "t7heZUb0KLTuVIJirbPcWDfrLxarCmSFOTDMRsMBpAu8Z"
+#ckey = "ejRTMayX2nhKZB2yNDCjDJWHz"
+#csecret = "3PGjS2HGvtEPY1DZh9feUDcMv0O02VyYk7Q5MLIS5MzibxwtuD"
+#atoken = "3045806279-RTecSDiDhevDXJ2YnbZ7BJKvUSylb6VvoDQqzDC"
+#asecret = "t7heZUb0KLTuVIJirbPcWDfrLxarCmSFOTDMRsMBpAu8Z"
+
+ckey = "h4mqB5AjlwQX3AXPCWLhGzZle"
+csecret = "BUwmRhpvkyweOVQNby9kJf2CU8S3LNWAZOzxoTwvnnpJJ508Ao"
+atoken = "3045806279-NGJNhwiXoiJsxZYAraz7E1EeUZh0yfhsmljWqCt"
+asecret = "NpWbo31FJ1KdBzVok0JzyEAm5Es0T9ndFT6NyxJEdHGds"
 
 class listener(StreamListener):
 
@@ -18,5 +23,5 @@ class listener(StreamListener):
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken,asecret)
-twitterstream = Stream(auth, listener())
-twitterstream.filter(track=["car"])
+twitter_stream = Stream(auth, listener())
+twitter_stream.filter(track=["car"])
